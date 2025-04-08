62fc24442e91de5c99b85907ae4d8b4f59645f9a
pujaPatil169 <marssitara6292@gmail.com>
2025-04-08 07:31:54 +0530

    Updates

--- src/app-entry.jsx
+++
@@ -1,4 +0,0 @@
-// Minimal entry point that imports your existing app
-import('./firebase/authService.js') // Keep your existing Firebase setup
-  .then(() => console.log('Firebase initialized'))
-  .catch(err => console.error('Firebase init error:', err));
